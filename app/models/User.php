9b5d6717ae390505279da4a18d53635fffc27ea1
John Bussey <jbussey365@yahoo.com>
2016-12-29 21:12:38 -0500

    got user, roles, and permissions working

--- app/models/User.php
+++ app/models/User.php
@@ -4,10 +4,15 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
+use Zizaco\Entrust\HasRole;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
-	use UserTrait, RemindableTrait;
+	use UserTrait, RemindableTrait, HasRole;
+
+	protected $fillable = array('first_name', 'last_name', 'email');
+
+	protected $appends = ['role_id'];
 
 	/**
 	 * The database table used by the model.
@@ -22,5 +27,21 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 * @var array
 	 */
 	protected $hidden = array('password', 'remember_token');
+
+	public function getRoleIdAttribute()
+	{
+		// if the user has at least one role attached...
+		if($this->roles->count())
+		{
+			return $this->roles->first()->id;
+		}
+	}
+
+	public function setRoleIdAttribute($role_id)
+	{
+		// if they're not save yet... save so that attachment can be made
+		if(!$this->id){$this->save();}
+		$this->roles()->sync([$role_id]);
+	}
 
 }
